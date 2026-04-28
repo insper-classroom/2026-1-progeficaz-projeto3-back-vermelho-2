22ce209a8cc23df4a886c25de666711689b6861a
Gabriel Kenji Nakahara Rocha <gaborocha.147@gmail.com>
2026-04-28 09:39:38 -0300

    API do servidor

---
+++ server.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template_string, request, jsonify
+import utils
+
+app = Flask(__name__)
+
+@app.route('/')
+def pagina_inicial():
+    return render_template_string('''
+        <p>Servidor rodando...</p>
+        <p>Acesse /cursos para acessar a API.</p>
+    ''')
+
+@app.route("/cursos", methods=["GET"])
+def get_cursos():
+    cursos = utils.get_all_cursos()
+    return jsonify(cursos)
+
+@app.route("/cursos/<titulo>", methods=["GET"])
+def get_curso(titulo):
+    curso = utils.get_one_curso(titulo)
+    
+    if curso:
+        return jsonify(curso)
+    
+    return jsonify({"erro": "Curso não encontrado"}), 404
+
+@app.route("/cursos", methods=["POST"])
+def novo_curso():
+    data = request.json
+    
+    titulo = data.get("titulo")
+    descricao = data.get("descricao")
+    local = data.get("local")
+    
+    if not titulo or not descricao or not isinstance(local, list):
+        return jsonify({"erro": "Dados incompletos para adicionar o curso"}), 400
+    
+    new_curso = utils.add_curso(titulo, descricao, local)
+    
+    if new_curso is None:
+        return jsonify({"erro": "Curso já existe!"}), 400
+    
+    return jsonify({"_id": new_curso}), 201
+
+@app.route("/cursos/<titulo>", methods=["PUT"])
+def update_curso(titulo):
+    data = request.json
+    
+    descricao = data.get("descricao")
+    local = data.get("local")
+
+    updated = utils.update_curso(titulo, descricao, local)
+    
+    if updated == 0:
+        return jsonify({"message": "Nada foi atualizado"}), 200
+    
+    return jsonify({"message": "Curso atualizado com sucesso!"})
+
+@app.route("/cursos/<titulo>", methods=["DELETE"])
+def deletar_curso(titulo):
+    deleted = utils.delete_curso(titulo)
+
+    if deleted == 0:
+        return jsonify({"erro": "Curso não encontrado"}), 404
+
+    return jsonify({"message": f"Curso '{titulo}' deletado com sucesso"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- utils.py
+++ utils.py
@@ -6,4 +6,55 @@ load_dotenv('.cred')
 
 client = MongoClient(os.getenv("MONGO_URI"))
 db = client["planejador_carreiras"]
-collection = db["cursos"]
+col = db["cursos"]
+
+def serialize(doc):
+    return {
+        "_id": str(doc["_id"]),
+        "titulo": doc["titulo"],
+        "descricao": doc["descricao"],
+        "local": doc["local"]
+    }
+
+def get_all_cursos():
+    cursos = list(col.find())
+    return [serialize(c) for c in cursos]
+
+def get_one_curso(nome):
+    curso = col.find_one({ "titulo": nome })
+    if curso:
+        return serialize(curso)
+    return None
+
+def add_curso(titulo, descricao, local):
+    if col.find_one({"titulo": titulo}):
+        return None
+    
+    novo_curso = {
+        "titulo": titulo,
+        "descricao": descricao,
+        "local": local
+    }
+
+    result = col.insert_one(novo_curso)
+    return str(result.inserted_id)
+
+def update_curso(titulo, descricao=None, local=None):
+    updated = {}
+    
+    if descricao is not None:
+        updated["descricao"] = descricao
+        
+    if local is not None:
+        updated["local"] = local
+        
+    if not updated:
+        return 0
+    
+    return col.update_one(
+        {"titulo": titulo},
+        {"$set": updated}
+    ).modified_count
+    
+def delete_curso(titulo):
+    return col.delete_one({"titulo": titulo}).deleted_count
